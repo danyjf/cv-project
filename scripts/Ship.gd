1c76797a925a52ff74b7e4223a179910257deac9
Daniel Francisco <76452190+danyjf@users.noreply.github.com>
2022-12-16 17:07:20 +0000

    Change player script to use mouse

--- scripts/Ship.gd
+++ scripts/Ship.gd
@@ -1,18 +1,50 @@
 extends KinematicBody
 
-var speed = 100
-var movement = Vector3.ZERO
-var rot_speed = 0.3
+export var move_speed = 100
+export var look_sensitivity = 5
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
-	if Input.is_action_pressed("w"):
-		movement = -transform.basis.z * speed
-	if Input.is_action_pressed("a"):
-		rotate_y(rot_speed * delta)
-	if Input.is_action_pressed("s"):
-		movement = transform.basis.z * speed
-	if Input.is_action_pressed("d"):
-		rotate_y(-rot_speed * delta)
+var min_look_angle = -90.0
+var max_look_angle = 90.0
+var mouse_delta = Vector2()
+var velocity = Vector3.ZERO
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _process(delta):
+	rotation_degrees.x -= mouse_delta.y * look_sensitivity * delta
+	rotation_degrees.x = clamp(rotation_degrees.x, min_look_angle, max_look_angle)
+	rotation_degrees.y -= mouse_delta.x * look_sensitivity * delta
 	
-	movement = move_and_slide(movement)
+	mouse_delta = Vector2.ZERO
+
+func _physics_process(_delta):
+	velocity = Vector3.ZERO
+	
+	var input = Vector2()
+	if Input.is_action_pressed("move_forward"):
+		input.y -= 1
+	if Input.is_action_pressed("move_backwards"):
+		input.y += 1
+	if Input.is_action_pressed("move_left"):
+		input.x -= 1
+	if Input.is_action_pressed("move_right"):
+		input.x += 1
+	input = input.normalized()
+	
+	var forward = transform.basis.z
+	var right = transform.basis.x
+	var relativeDir = (forward * input.y + right * input.x)
+	
+	velocity = relativeDir * move_speed
+
+	velocity = move_and_slide(velocity)
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		mouse_delta = event.relative
+	if Input.is_action_just_released("esc"):
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
